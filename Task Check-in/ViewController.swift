2265aef49a2bff75bc067ead716fd968fdb7c2b1
nrewik <nrewik@outlook.com>
2015-12-22 14:55:52 +0700

    Add Xcode project

---
+++ Task Check-in/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Task Check-in
+//
+//  Created by Nutchaphon Rewik on 22/12/2015.
+//  Copyright © 2015 Nutchaphon Rewik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
